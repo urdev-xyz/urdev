7c7804c16a44e23c7b9f5bb9b7112fff7dc233bf
Walker Frederick <walker@urdev.xyz>
2019-07-05 18:26:38 -0400

    added final copy to service page

--- src/pages/services.js
+++ src/pages/services.js
@@ -13,7 +13,7 @@ const Services = () => (
             <div className="service-header">
                 <div className="service-header-left">
                     <h1>Our Services</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <p>Every Project is unique and has to be approached strategically in order to achieve the desired final product. That is exactly why we never offer packages or pre-bundled sets of services. Instead we design a set of services that fits your needs exactly and doesn’t leave you paying for something you don’t need.</p>
                 </div>
                 <div className="service-header-right">
                     <div id="service-header-right-image-1" style={{background: `url(${appdev})`}} className="service-header-right-image fade-in-fast">
@@ -24,53 +24,57 @@ const Services = () => (
             </div>
             <div className="service-section">
                 <div className="service-section-left">
-                    <h1>Web Services</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <h1>Web</h1>
                 </div>
                 <div className="service-section-right">
                     <div className="service-section-item">
                         <h1>Web Design</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <p>Your website is a crucial way for your business to illustrate it’s image and brand to the world, Which is why it is so crucial to nail the design. Whether you need a website designed around an existing brand or want to start from scratch, all of our websites are guaranteed to show your customers just what you are all about.</p>
                     </div>
                     <div className="service-section-item">
                         <h1>Web Development</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <p>Websites need to be as robust under the hood as they are usable to your customers. We only use the latest in Web Development technologies to insure your website is fast, stable, and always working as expected. All of our Websites ship with a CMS ( Content Management System ) allowing you to edit aspects of the website without ever touching a line of code.</p>
                     </div>
                     <div className="service-section-item">
                         <h1>Search Engine Optimization</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <p>Arguably the most important aspect of any website is its ability to be found by customers. Our websites are always optimized for search engine rank but if you find that your business needs more assistance, We offer several methods to propel your website to the top of search results.</p>
                     </div>
                 </div>
             </div>
             <div className="service-section">
                 <div className="service-section-left">
-                    <h1>App Services</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <h1>Mobile App</h1>
                 </div>
                 <div className="service-section-right">
                     <div className="service-section-item">
                         <h1>App Design</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <p>The best designed apps need an intuitive and seamless interface to encourage interactions from the user. All of our applications take this to heart and provide a well designed layout that is fast and effective in providing your service.</p>
                     </div>
                     <div className="service-section-item">
                         <h1>App Development</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <p>Using the latest in app development technologies we have streamlined the development process significantly, allowing for cross platform development and decreased development times. Our applications are always ready to be published on android and IOS so you never leave any users in the dark.</p>
                     </div>
                 </div>
             </div>
             <div className="service-section">
                 <div className="service-section-left">
-                    <h1>Design Services</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <h1>Design</h1>
                 </div>
                 <div className="service-section-right">
                     <div className="service-section-item">
                         <h1>Logo Design</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <p>Your company's logo is at the heart of how it identifies itself to customers. A great logo should capture who and what you do while remaining simple and memorable. Our approach to logo design is based on lots of iterations and refinement until you are happy with the final product.</p>
                     </div>
+                </div>
+            </div>
+            <div className="service-section">
+                <div className="service-section-left">
+                    <h1>Social</h1>
+                </div>
+                <div className="service-section-right">
                     <div className="service-section-item">
-                        <h1>Idenity Design</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <h1>Social Media Management</h1>
+                        <p>How your company portrays itself on social media is an extremely overlooked aspect of doing business in the digital age. We aim to accurately display you company on multiple platforms, engage your customers, and create a source of new customers that could otherwise not be exposed to your business. </p>
                     </div>
                 </div>
             </div>
